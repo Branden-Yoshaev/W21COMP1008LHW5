e9a4224ea8f05982678f97088760533552cbda31
Branden-Yoshaev <bsyoshae@lakeheadu.ca>
2021-02-10 11:29:54 -0500

    Part 1 to test 1

---
+++ src/groceryItem.java
@@ -0,0 +1,41 @@
+
+public class groceryItem {
+    private String name;
+    private double price;
+    private String category;
+
+    public groceryItem(String name, double price, String category) {
+        setName(name);
+        setPrice(price);
+        setCategory(category);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name.length()>=2)
+            this.name = name;
+        else
+            throw new IllegalArgumentException("name must have at least 2 characters")
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+
+}
